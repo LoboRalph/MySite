34775d5b564a44b650c025e114dbc022d87c5e36
Francisco Lucas <semcriatividade6556@gmail.com>
2015-09-07 01:50:35 -0300

    added design

---
+++ code/php/view/head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head lang="pt-br">
+        <title>Meu Site - alpha</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="code/css/reset.css">
+        <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Righteous' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="code/css/nav.css">
+        <script type="text/javascript" src="../src/jquery/jquery2x.js"></script>
+    </head>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php require_once("code/php/view/head.php"); ?>
+
+<body>
+    <section id="about">
+        <nav id="nav">
+                <a>About</a>
+                <a>Contatc</a>
+                <a>Project</a>
+        </nav>
+        <div id="hi">
+            <h1 id="ti">HELLO</h1>
+            <h2 id="tit">my name is Francisco Lucas</h2>
+        </div>
+    </section>
+    <section id="exp">
+    
+    </section>
+    <section id="cont">
+    
+    </section>
+</body>
+<footer>
+
+</footer>
